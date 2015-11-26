c952f4725f69663ca95c47f1a0c44dcaf101bde5
Martins Liberts <martins.liberts@csb.gov.lv>
2015-11-26 15:44:34 +0200

    s2 with correction

--- R/s2.R
+++ R/s2.R
@@ -1,6 +1,8 @@
 # S^2 estimation
 s2 <- function(y, w = rep(1, length(y))) {
   N <- sum(w)
-  s2 <- (sum(y ^ 2 * w) - sum(y * w) ^ 2 / N) / (N - 1)
+  n <- length(y)
+  s2 <- (N - 1) / N * n / (n - 1) *
+    (sum(y ^ 2 * w) - sum(y * w) ^ 2 / N) / (N - 1)
   return(s2)
 }
